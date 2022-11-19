fd05661b9ea0e6af345c6e9df247d216792cf10f
Stanislav Kolychev <101045430+stanislavkolychev@users.noreply.github.com>
2022-11-19 14:30:33 +0300

    Delete 5.2.py

--- lesson_5/5.2.py
+++
@@ -1,7 +0,0 @@
-# Создать текстовый файл (не программно),
-# сохранить в нём несколько строк, выполнить подсчёт строк и слов в каждой строке.
-
-with open ("text2.txt", "r", encoding='utf-8') as f_obj:
-    useful = [f'{line}, {count.strip()} - {len(count.split())} слов'
-              for line, count in enumerate(f_obj, 1)]
-    print(*useful, f"Всего строк - {len(useful)}.", sep="\n")
